ac0ce4fe49ea2334ef7d87ad5489d52f447bfce2
Sharath Raj Macharla <smacharla@intralinks.com>
2019-10-10 20:52:19 +0530

    testing push commands with new code

--- complete/Sample.groovy
+++ complete/Sample.groovy
@@ -1,52 +1,20 @@
-multibranchPipelineJob('example') {
-    branchSources {
-        git {
-            id('git') // IMPORTANT: use a constant and unique identifier
-            remote('ssh://git@stash.intralinks.com:7999/qe/qe-pom.git')
-            credentialsId('git')
-            includes('JENKINS-*')
-        }
-    }
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(20)
-        }
-    }
-}
-
-job ('Git Move'){
-parameters {
-         stringParam('BUILD_TAG', '', '')
-     }
-     label('pr-merged-builds')
-     scm {
+job('pull-request-job') {
+    scm {
         git {
             remote {
-                name('test3')
+                name 'origin'
                 url('https://github.com/msharathraj/SampleTest.git')
             }
-            branch('master')
+
+            branch '**/pull-requests/**'
+
             extensions {
                 mergeOptions {
-                    remote('origin')
-                    branch('master')
+                    remote 'origin'
+                    branch 'master'
+					localBranch 'master'
                 }
             }
         }
     }
-	steps {
-	
-		shell('''
-	         env
-	         
-		echo "Entered steps to do"
-	        
-	      echo "Generating the versionlock file and pushing to the repo"
-	      bat   git pull origin test3
-	      bat   git add --A
-	      bat   git status | grep "nothing to commit"
-	      bat   git commit -am "Committing changes caused by 1.0.0"
-	      bat   git push origin master
-	    '''.stripIndent())
-     }
-}	
+}
