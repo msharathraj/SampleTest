acb9674b726fc1dc2be73c074f989c8f55e84c88
Sharath Raj Macharla <smacharla@intralinks.com>
2019-10-10 19:44:39 +0530

    testing branch checkout

--- complete/Sample.groovy
+++ complete/Sample.groovy
@@ -13,3 +13,23 @@ multibranchPipelineJob('example') {
         }
     }
 }
+
+job ('Git Move'){
+parameters {
+         stringParam('BUILD_TAG', '', '')
+     }
+     label('pr-merged-builds')
+     scm {
+        git {
+          remote {
+            url('ssh://git@stash.intralinks.com:7999/qe/qe-pom.git')
+            branch("develop")
+            credentials(gitCreds)
+            extensions {
+                localBranch('master')
+                wipeOutWorkspace()
+            }
+          }
+        }
+    }
+}	
