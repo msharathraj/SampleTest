c85e82174ba5c20fe09384e0d4982347831ad346
Sharath Raj Macharla <smacharla@intralinks.com>
2019-10-11 16:34:54 +0530

    test

--- complete/Sample.groovy
+++ complete/Sample.groovy
@@ -1,4 +1,4 @@
-job("testjob") {
+job("Merge-Release") {
      scm {
         git {
           remote {
@@ -11,7 +11,31 @@ job("testjob") {
         }
     }
     steps {
-	      batchFile('echo Hello World!')
+	     batchFile('echo Hello World!')
+	     batchFile('git status')
+	     batchFile('git branch')
+		 
+		 maven('mvn clean install')
+		 maven('mvn')
+		 
+	     
+     }
+}
+
+job("Merge-Master") {
+     scm {
+        git {
+          remote {
+            url('https://github.com/msharathraj/SampleTest.git')
+            branch("master")
+            extensions {
+                localBranch('test3')
+            }
+          }
+        }
+    }
+    steps {
+	     batchFile('echo Hello World!')
 	     batchFile('git status')
 	     batchFile('git branch')
 	     
