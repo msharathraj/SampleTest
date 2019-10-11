6be05dd19f613ccb1279ae253abf72e4c2d7a272
Sharath Raj Macharla <smacharla@intralinks.com>
2019-10-11 17:02:02 +0530

    made changes

--- complete/Sample.groovy
+++ complete/Sample.groovy
@@ -1,43 +1,96 @@
-job("Merge-Release") {
+gitCreds = 'git'
+listView('Git-Flow-Jobs') {
+	description("All Git Flow Jobs.")
+    filterBuildQueue(true)
+    filterExecutors(true)
+    jobs {
+        regex('.*-Git')
+    }
+    columns {
+        status()
+     	weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
+job("Merge-Release-Git") {
      scm {
         git {
           remote {
-            url('https://github.com/msharathraj/SampleTest.git')
-            branch("test3")
+			url('https://github.com/msharathraj/SampleTest.git')
+            branch("develop")
             extensions {
-                localBranch('test3')
+                localBranch('develop')
             }
           }
         }
     }
     steps {
 	     batchFile('echo Hello World!')
-	     batchFile('git status')
 	     batchFile('git branch')
-		 
-		 maven('mvn clean install')
-		 maven('mvn')
-		 
-	     
-     }
+	     batchFile('git branch release')
+	     batchFile('git checkout release')
+	     batchFile('git merge develop')
+	     batchFile('git push')
+		 triggers {
+			githubPush()
+		}
+		 mavenJob('mvn clean install') {
+			postBuildSteps('SUCCESS') {
+				batchFile("echo 'run after Maven'")
+			}
+		}
+	 }
 }
 
-job("Merge-Master") {
+job("Merge-Master-Git") {
      scm {
         git {
           remote {
             url('https://github.com/msharathraj/SampleTest.git')
-            branch("master")
+            branch("develop")
             extensions {
-                localBranch('test3')
+                localBranch('develop')
             }
           }
         }
     }
     steps {
 	     batchFile('echo Hello World!')
-	     batchFile('git status')
 	     batchFile('git branch')
-	     
-     }
+	     batchFile('git checkout master')
+	     batchFile('git merge develop')
+		 batchFile('git push')
+		 triggers {
+			githubPush()
+		}
+	 }
+}
+
+job("Tag-Creation-Git") {
+     scm {
+        git {
+          remote {
+            url('https://github.com/msharathraj/SampleTest.git')
+            branch("develop")
+            extensions {
+                localBranch('develop')
+            }
+          }
+        }
+    }
+    steps {
+	     batchFile('echo Hello World!')
+	     batchFile('git branch')
+	     batchFile('git checkout master')
+	     batchFile('git merge develop')
+		 batchFile('git push')
+		 triggers {
+			githubPush()
+		}
+
+	 }
 }
