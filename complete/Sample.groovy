fdd40bdb4d9bb3101632fe809a120220aaf5dfdd
Sharath Raj Macharla <smacharla@intralinks.com>
2019-10-11 16:22:15 +0530

    sada

---
+++ complete/Sample.groovy
@@ -0,0 +1,43 @@
+
+job("Merge-Release") {
+     scm {
+        git {
+          remote {
+            url('https://github.com/msharathraj/SampleTest.git')
+            branch("master")
+            extensions {
+                localBranch('test3')
+            }
+          }
+        }
+    }
+    steps {
+	     batchFile('echo Hello World!')
+	     batchFile('git status')
+	     batchFile('git branch')
+		 
+		 maven('mvn clean install')
+		 
+	     
+     }
+}
+
+job("Merge-Master") {
+     scm {
+        git {
+          remote {
+            url('https://github.com/msharathraj/SampleTest.git')
+            branch("master")
+            extensions {
+                localBranch('test3')
+            }
+          }
+        }
+    }
+    steps {
+	     batchFile('echo Hello World!')
+	     batchFile('git status')
+	     batchFile('git branch')
+	     
+     }
+}
